d6d09afcd3771a0eaa1ce96590506a84b1cc8d18
jdeans <joshua.deans21@gmail.com>
2018-10-27 15:11:31 -0700

    Merge branch 'master' of csil-git1.cs.surrey.sfu.ca:ewtempes/Carpool
    # Conflicts:
    #	.idea/Carpool.iml

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function landing(){
+        return view('landing');
+    }
+
+    public function login_page(){
+        return view('pages.login');
+    }
+
+    public function signup_page(){
+        return view('pages.signup');
+    }
+}
