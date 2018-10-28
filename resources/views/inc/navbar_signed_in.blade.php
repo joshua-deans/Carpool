4d77f84c500c956e2d9d6cfa3a098ef0a76b3ff0
jdeans <joshua.deans21@gmail.com>
2018-10-28 15:12:43 -0700

    Merge branch 'master' of csil-git1.cs.surrey.sfu.ca:ewtempes/Carpool
    # Conflicts:
    #	.idea/Carpool.iml

--- resources/views/inc/navbar_signed_in.blade.php
+++ resources/views/inc/navbar_signed_in.blade.php
@@ -8,18 +8,17 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">Carpool</a>
+            <a class="navbar-brand" href="#">Carpool</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="#">About</a></li>
-                <li><a href="#">How it works</a></li>
+
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="/login">Login</a></li>
-                <li><a href="/signup">Sign Up</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="/">Sign Out</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -3,6 +3,7 @@
 @section('stylesheet')
     <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_EIWb_yAvbLmnjYU4qHxvzWlcGKU-jeA&libraries=places"></script>
     <style>
         #map {
             height: 100%;
@@ -16,24 +17,21 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4">
+                <h3>Plan your Commute!</h3>
                 <form>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="inputFirstName" placeholder="First Name">
+                        <input type="text" class="form-control" id="input-origin" placeholder="Origin">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="inputLastName" placeholder="Last Name">
+                        <input type="text" class="form-control" id="input-dest" placeholder="Destination">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="date" name="date" placeholder="MM/DD/YYYY">
+                        <label class="radio-inline"><input type="radio" name="userType" value="passenger" checked>Passenger</label>
+                        <label class="radio-inline"><input type="radio" name="userType" value="driver">Driver</label>
                     </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <input type="tel" class="form-control" id="inputPhone" placeholder="Phone #">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign Up</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
+                <br>
             </div>
             <div class="col-8">
                 <div id="map" style="height:450px;"></div>
@@ -46,16 +44,11 @@
         // failed.", it means you probably did not give permission for the browser to
         // locate you.
         var map, infoWindow;
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 49.2827, lng: -123.1207},
-                zoom: 11
-            });
-            // infoWindow = new google.maps.InfoWindow;
 
+        function setUpGeolocation() {
             // Try HTML5 geolocation.
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
+                navigator.geolocation.getCurrentPosition(function (position) {
                     var pos = {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude
@@ -65,7 +58,7 @@
                     // infoWindow.setContent('Location found.');
                     // infoWindow.open(map);
                     map.setCenter(pos);
-                }, function() {
+                }, function () {
                     // handleLocationError(true, infoWindow, map.getCenter());
                 });
             } else {
@@ -73,6 +66,79 @@
                 // handleLocationError(false, infoWindow, map.getCenter());
             }
         }
+
+        function AutocompleteDirectionsHandler(map) {
+            this.map = map;
+            this.originPlaceId = null;
+            this.destinationPlaceId = null;
+            this.travelMode = 'DRIVING';
+            var originInput = document.getElementById('input-origin');
+            var destinationInput = document.getElementById('input-dest');
+            this.directionsService = new google.maps.DirectionsService;
+            this.directionsDisplay = new google.maps.DirectionsRenderer;
+            this.directionsDisplay.setMap(map);
+
+            var originAutocomplete = new google.maps.places.Autocomplete(
+                originInput);
+            var destinationAutocomplete = new google.maps.places.Autocomplete(
+                destinationInput);
+
+
+            this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+            this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+        }
+
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 49.2827, lng: -123.0197},
+                zoom: 11,
+                disableDefaultUI: true
+            });
+            // infoWindow = new google.maps.InfoWindow;
+
+            setUpGeolocation();
+            new AutocompleteDirectionsHandler(map);
+        }
+
+        AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+            var me = this;
+            autocomplete.bindTo('bounds', this.map);
+            autocomplete.addListener('place_changed', function() {
+                var place = autocomplete.getPlace();
+                if (!place.place_id) {
+                    window.alert("Please select an option from the dropdown list.");
+                    return;
+                }
+                if (mode === 'ORIG') {
+                    me.originPlaceId = place.place_id;
+                } else {
+                    me.destinationPlaceId = place.place_id;
+                }
+                me.route();
+            });
+
+        };
+
+        AutocompleteDirectionsHandler.prototype.route = function() {
+            if (!this.originPlaceId || !this.destinationPlaceId) {
+                return;
+            }
+            var me = this;
+
+            this.directionsService.route({
+                origin: {'placeId': this.originPlaceId},
+                destination: {'placeId': this.destinationPlaceId},
+                travelMode: this.travelMode
+            }, function(response, status) {
+                if (status === 'OK') {
+                    me.directionsDisplay.setDirections(response);
+                } else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            });
+        };
+
 
         function handleLocationError(browserHasGeolocation, infoWindow, pos) {
             infoWindow.setPosition(pos);
@@ -84,8 +150,7 @@
     </script>
 
 
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=
+    <script async defer src="https://maps.googleapis.com/maps/api/js?v=weekly&key=
 AIzaSyC_EIWb_yAvbLmnjYU4qHxvzWlcGKU-jeA
-&callback=initMap"
-            ></script>
+&callback=initMap"></script>
 @endsection
