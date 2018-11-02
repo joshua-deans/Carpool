e5dac0c7a89a156c5c3d0a35f687234cd3942bb3
xbei <xbei@sfu.ca>
2018-11-01 22:34:05 -0700

    Merge branches '14-display-commute' and 'master' of https://csil-git1.cs.surrey.sfu.ca/ewtempes/Carpool into 14-display-commute
    # Conflicts:
    #	resources/views/inc/navbar_signed_in.blade.php
    #	routes/web.php
    
    fix problem: two profile links on nav bar

--- resources/views/routes/index.blade.php
+++ resources/views/routes/index.blade.php
@@ -6,17 +6,18 @@
 
 @section('content')
     @include('inc.navbar_signed_in')
-    <div class="well"><h1> My Routes</h1></div>
-    @if (count($routes) > 0)
-        @foreach($routes as $route)
-            <div class="well">
-                <h3><a href="/Routes/{{$route->rideId}}">route id: {{$route->rideId}}</a></h3>
-                <small>date time:{{$route->carpoolDateTime}}</small>
-            </div>
-        @endforeach
-        {{$routes->links()}}
-    @else
-        <div class="well"><p> No routes found</p></div>
-    @endif
-
+    <div id="route_list">
+        <div class="well"><h1> My Routes</h1></div>
+        @if (count($routes) > 0)
+            @foreach($routes as $route)
+                <div class="well">
+                    <h3><a href="/Routes/{{$route->rideId}}">route id: {{$route->rideId}}</a></h3>
+                    <small>date time:{{$route->carpoolDateTime}}</small>
+                </div>
+            @endforeach
+            {{$routes->links()}}
+        @else
+            <div class="well"><p> No routes found</p></div>
+        @endif
+    </div>
 @endsection
