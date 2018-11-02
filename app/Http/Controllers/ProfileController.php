b014941fb8c2ffc29ae9173e96e9190f83ff8c3e
Ethan Li <ethanhaoli@gmail.com>
2018-11-01 19:52:15 -0700

    Merge branch 'master' of csil-git1.cs.surrey.sfu.ca:ewtempes/Carpool into 18-fix-login-nav-bar
    # Conflicts:
    #	routes/web.php
    #   add proper .env file

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Image;
+
+class ProfileController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        $user = auth()->user();
+        return view('profile.profile')->with('user', $user);
+    }
+
+    public function edit(){
+        $user = auth()->user();
+        return view('profile.edit')->with('user', $user);
+    }
+
+    public function update(Request $request){
+        $this->validate($request, array(
+            'name' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ));
+        $user = auth()->user();
+        $updateUser = User::find($user->id);
+        $updateUser->name = $request->input('name');
+        $updateUser->description = $request->input('about');
+//        $updateUser->phone# = $request->input('phone');
+
+        if($request->hasFile('picture')){
+            $image= $request->file('picture');
+            $filename = time() . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(200, 200)->save( public_path('images/' . $filename ) );
+            $updateUser->profilePicture= $filename;
+        };
+
+        $updateUser->save();
+
+        return redirect('/profile')->with('user', $user);
+    }
+}
---
+++ resources/views/profile/profile.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('stylesheet')
+    <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+    @include('inc.navbar_signed_in')
+    <div class="container">
+        <div class="row">
+        <div class="profileHeader">
+            <div class="col-md-3">
+                @if ($user->profilePicture)
+                    <img src="{{asset('images/'.$user->profilePicture)}}" class="profilePicture">
+                @else
+                    <img src="{{URL::asset('imgs/emptyProfile.jpg')}}" class="profilePicture">
+                @endif
+            </div>
+            <div class="profileNameInfo col-md-6">
+                <h3>
+                    Name:
+                    <?php
+                    echo $user->name
+                    ?>
+                </h3>
+                <h3>
+                    Email:
+                    <?php
+                    echo $user->email;
+                    ?>
+                </h3>
+                <h3>
+                    Phone:
+                    <?php
+                    echo $user->phone;
+                    ?>
+                </h3>
+                <h3>
+                    About:
+                    <?php
+                    echo $user->description;
+                    ?>
+                </h3>
+                <h3>
+                    Birthday:
+                    <?php
+                    echo $user->birthday;
+                    ?>
+                </h3>
+                <h3>
+                    Rating:
+                    <?php
+                    echo $user->avgRating;
+                    ?>
+                </h3>
+            </div>
+        </div>
+
+        </div>
+        <div class="row">
+            <a href="profile/edit" class="btn btn-light">Edit</a>
+        </div>
+    </div>
+
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
+@endsection
