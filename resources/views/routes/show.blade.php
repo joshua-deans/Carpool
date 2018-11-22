17ee5aacf75da40a2fca8586ac1e5cdcab6c3e32
mta86 <mta86@sfu.ca>
2018-11-21 22:03:10 -0800

    display location names on myRoute page

--- resources/views/routes/show.blade.php
+++ resources/views/routes/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('stylesheet')
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js">
         var dateString = moment.unix(value).format("MM/DD/YYYY");
 
@@ -16,14 +17,10 @@
         <div class="left">
             <a href = "/Routes" class = "btn btn-default">Go Back</a>
             <div id="coords" style="display: none;">{{$routes->coords}}</div>
-            <h1> start: </h1>
-            <p id="oriName"></p>
-            <h1> end: </h1>
-            <p id="destName"></p>
-
 
             <ul class="list-group">
-
+                <li class="list-group-item"><h4>From:</h4><p id="oriName"></p></li>
+                <li class="list-group-item"><h4>To:</h4><p id="destName"></p></li>
                 <li class="list-group-item" id="datetime">datetime: <?php echo gmdate("l jS F Y h:i:s A", $routes->datetime);?></li>
                 <li class="list-group-item">driver name:{{$driver[0]->name}}</li>
                 @if (count($passenger) > 0)
@@ -31,10 +28,7 @@
                 @else
                     <li class="list-group-item">passenger name:No passenger</li>
                 @endif
-
-                <li class="list-group-item">Location: need to transfer json int to name {{$routes->coords}}</li>
             </ul>
-
             {!! Form::open(['action' => ['RoutesController@destroy', $routes->rideId], 'method' => 'post']) !!}
             {{Form::hidden('_method', 'delete')}}
             {{Form::button('<i class="fas fa-trash">Delete</i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-md'])}}
@@ -43,39 +37,47 @@
     </div>
     <script>
         var coordJSON = document.getElementById("coords").textContent;
-        var Oricoords = JSON.parse(coordJSON);
-        var oriLat = coordJSON.oriLat;
-        var oriLng = coordJSON.oriLng;
-        var destLat = coordJSON.destLat;
-        var destLng = coordJSON.destLng;
+        var oricoords = JSON.parse(coordJSON);
+        var oriLat = oricoords.oriLat;
+        var oriLng = oricoords.oriLng;
+        var destLat = oricoords.destLat;
+        var destLng = oricoords.destLng;
         var oriString = oriLat + ',' + oriLng;
         var destString = destLat + ',' + destLng;
+        geocode(oriString,"oriName");
+        geocode(destString,"destName");
 
-        function geocodeLatLng(geocoder) {
-            var input = document.getElementById('latlng').value;
-            var latlngStr = input.split(',', 2);
-            var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        function geocode(start, divID){
+            axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+                params:{
+                    latlng:start,
+                    key: 'AIzaSyCgBuOiCTu4lSCitAlk-YxtZ_Aw6KRIqxU'
+                }
+            })
+                .then(function(response){
+                    document.getElementById(divID).innerHTML=
+                        response.data.results[0].formatted_address;
+                })
+                .catch(function(error){
+                    console.log(error);
+            })
+        }
+
+
+        /*function geocodeLatLng(geocoder) {
+            var latlng = {lat: parseFloat(geocoder[0]), lng: parseFloat(geocoder[1])};
             geocoder.geocode({'location': latlng}, function(results, status) {
                 if (status === 'OK') {
-                    if (results[0]) {
-                        map.setZoom(11);
-                        var marker = new google.maps.Marker({
-                            position: latlng,
-                            map: map
-                        });
-                        infowindow.setContent(results[0].formatted_address);
-                        infowindow.open(map, marker);
-                    } else {
-                        window.alert('No results found');
-                    }
-                } else {
+                    alert(results[0].formatted_address);
+                }
+                else {
                     window.alert('Geocoder failed due to: ' + status);
                 }
             });
         }
         //var origin = document.getElementById("oriName");
-        //use reverse geolocation to find the names of the coords once coords are extracted from the data base;
+        //use reverse geolocation to find the names of the coords once coords are extracted from the data base;*/
+
     </script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?v=weekly&key=
-AIzaSyC_EIWb_yAvbLmnjYU4qHxvzWlcGKU-jeA&libraries=places&callback=geocodeLatLng"></script>
+
 @endsection
